fb23947b1e6917bce926843d8e3d5178f7b84bf2
Umair <156466199+Mohammed-Umair-Kalim@users.noreply.github.com>
2024-09-21 15:30:35 +0500

    Update 03-milestone.js

--- TypeScript-JavaScript/03-milestone.js
+++ TypeScript-JavaScript/03-milestone.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <style>
             #div2{
     background-color: rgba(182, 182, 182, 0.74);
-    font-size:x-large;
+    font-size:xx-large;
      padding-block-end: 150px;
      margin:100px;
 
